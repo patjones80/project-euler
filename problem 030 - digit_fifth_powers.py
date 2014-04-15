2774633da782ec6aa7b916baf5cdf01936552844
patjones80 <patjones80@gmail.com>
2014-04-15 00:24:48 -0400

    Problem 30.
    More (useless?) number theory; whatever, it's fun stuff.

---
+++ problem 030 - digit_fifth_powers.py
@@ -0,0 +1,39 @@
+
+
+# project euler 30
+
+# these numbers turn out to be related to "narcissistic numbers"
+
+# see this for an interesting discussion:
+# http://mathworld.wolfram.com/NarcissisticNumber.html
+
+import time
+
+N = 354294
+m = 5
+total = 0
+
+start = time.clock()
+
+for j in range(2, N+1):
+
+    jlist = list(str(j))
+    n = 0
+
+    for k in range(0, len(jlist)):
+        digit = int(jlist[k])
+        n = n + digit**m
+    
+    if j == n:
+        print('%s: %s' % (j, n))
+        total = total + j
+
+print('\nsum: %s' % total)        
+print('\ntime: %s' % str(time.clock()-start))
+    
+
+            
+        
+    
+    
+
